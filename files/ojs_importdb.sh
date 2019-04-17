51aef684e8955c79bd62a2c749d69d12a98c5bb7
Cody Bennett <codybennett@ou.edu>
2019-04-17 09:24:30 -0500

    Add MySQL command: CREATE DATABSE IF NOT EXISTS $PKPDB; Add Table import stats

--- files/ojs_importdb.sh
+++ files/ojs_importdb.sh
@@ -9,7 +9,7 @@ if [  -z "$1" ]; then
 ojs_importdb.sh performs a database import to an OJS site.
 
 Usage: ojs_importdb.sh \$SITEPATH
-            
+
 \$SITEPATH  OJS target site for sql import (eg. /srv/example).
 USAGE
 
@@ -18,13 +18,19 @@ fi
 
 SITEPATH=$1
 
+echo "Creating $SITEPATH database if not alreay present"
+sudo -u nginx bash -c "mysql -h $PKPDBHOST -u $PKPUSER -p$PKPPASS -e 'CREATE DATABASE IF NOT EXISTS $PKPDB'"
+
 echo "Importing $SITEPATH database"
 
 ## Grab the basename of the site to use in a few places.
 SITE=$(basename "$SITEPATH")
 
+
 ## Import sql-dump
-sudo -u nginx bash -c "mysql -h $PKPDBHOST -u $PKPUSER -p$PKPPASS -D $PKPDB < $SITEPATH/db/ojs_${SITE}_dump.sql"
+sudo -u nginx bash -c "mysql -h $PKPDBHOST -u $PKPUSER -p$PKPPASS $PKPDB < $SITEPATH/db/ojs_${SITE}_dump.sql"
+
+echo "Imported $(sudo -u nginx bash -c "mysql --disable-column-names -B -h $PKPDBHOST -u $PKPUSER -p$PKPPASS $PKPDB -e 'select count(*) from information_schema.tables where table_schema = database();'") Tables"
+
 
 echo "Finished importing $SITEPATH database."
-
